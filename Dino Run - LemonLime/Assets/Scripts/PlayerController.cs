8c43e4da536d555fa0f20ffdf758c861cff3d6d4
K-auiwfhwegjku <kaijie.zhou@digipen.edu>
2020-01-31 16:28:54 -0800

    DOing some stuff

--- Dino Run - LemonLime/Assets/Scripts/PlayerController.cs
+++ Dino Run - LemonLime/Assets/Scripts/PlayerController.cs
@@ -11,14 +11,22 @@ public class PlayerController : MonoBehaviour
     float speedMultiplier = 0f;
 
     bool isJump;
-    bool isFall;
+    bool isQuickFall;
+    bool isGrounded;
 
+    public float groundPosition = 0;
+    //Jump cooldown timer
+    float JumpCooldown = 0.1f;
     //Timer for time key held to make jump height different
     float JumpHeightTimer = 0f;
+    //original positionto check if climax of jump
+    Vector3 OriginalPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        OriginalPosition = gameObject.transform.position;
+        //set groundPosition to top of ground platform Y.
+
     }
 
     // Update is called once per frame
@@ -26,18 +34,41 @@ public class PlayerController : MonoBehaviour
     {
         //Key Events: Up / Space and down arrow
         isJump = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Space);
-        isFall = Input.GetKey(KeyCode.DownArrow);
-            
+        isQuickFall = Input.GetKey(KeyCode.DownArrow);
+
     }
 
     private void FixedUpdate()
     {
-        if (isJump) {
+        JumpCooldown -= Time.fixedDeltaTime;
+        if (isJump && isGrounded && JumpCooldown == 0f)
+        {
             //record amount of time key held down for then multiply temp variable of how high to jump
-            
-            //Use Rigidbody velocity and vector3.
+            JumpHeightTimer += Time.fixedDeltaTime;
+            //Is player on the ground?
+            isGrounded = transform.position.y == groundPosition;
+            //once JumpHeightTimer is past a minimum value, multiply JumpHeightTimer to amount for jump. 
 
+            //Temp: Minimum 0.5f for timer, 5 for jump height  
+            if (JumpHeightTimer >= .5f)
+            {
+                //Use Rigidbody velocity and vector3.
+                gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, JumpHeightTimer * 5 * baseSpeed, 0);
+            }
+            //once reached highest point of jump, start falling at accurate velocity.
+
+            //Temp: same as with jumping.Climax of jump capped 1 second.
+            if ((gameObject.transform.position.y - OriginalPosition.y) == (JumpHeightTimer * 5 * 1f))
+            {
+                gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, (JumpHeightTimer * 5) * baseSpeed * -1, 0);
+            }
         }
 
+        if (isQuickFall)
+        {
+            //Temp: fall down at twice the rate of normal.
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, (JumpHeightTimer * 5) * baseSpeed * -2, 0);
+        }
+        
     }
 }
