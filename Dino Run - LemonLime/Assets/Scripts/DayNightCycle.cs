4666e84429bdbbe2f12bd1f33bc2b9ab8fcb0b3b
K-auiwfhwegjku <kaijie.zhou@digipen.edu>
2020-02-06 15:44:39 -0800

    Added a DayNightCYle script, not much progress on it yet

---
+++ Dino Run - LemonLime/Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DayNightCycle : MonoBehaviour
+{
+    int score = GameObject.Find("GameControl").GetComponent<GameControl>().GetScore();
+    GameObject groundPrefab = PrefabUtility.LoadPrefabContents(AssetDatabase.GetAssetPath(GameObject.FindWithTag("Ground")));
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
