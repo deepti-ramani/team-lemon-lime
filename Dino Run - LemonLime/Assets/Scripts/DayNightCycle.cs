ab5beed3c38646fcd60a834838c84d9ff162ef24
K-auiwfhwegjku <kaijie.zhou@digipen.edu>
2020-02-07 15:39:38 -0800

    edf

--- Dino Run - LemonLime/Assets/Scripts/DayNightCycle.cs
+++ Dino Run - LemonLime/Assets/Scripts/DayNightCycle.cs
@@ -7,6 +7,7 @@ public class DayNightCycle : MonoBehaviour
 {
     int score = GameObject.Find("GameControl").GetComponent<GameControl>().score;
     GameObject groundPrefab = PrefabUtility.LoadPrefabContents(AssetDatabase.GetAssetPath(GameObject.FindWithTag("Ground")));
+    GameObject obstaclePrefab = PrefabUtility.LoadPrefabContents(AssetDatabase.GetAssetPath(GameObject.FindWithTag("Obstacle")));
     // Start is called before the first frame update
     void Start()
     {
