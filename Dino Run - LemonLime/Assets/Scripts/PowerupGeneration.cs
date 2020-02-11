e7bde26f044dec611f21d85f1880b927b4b37415
Deepti <deepti.ramani@digipen.edu>
2020-02-11 15:39:02 -0800

    updated powerup gen

--- Dino Run - LemonLime/Assets/Scripts/PowerupGeneration.cs
+++ Dino Run - LemonLime/Assets/Scripts/PowerupGeneration.cs
@@ -7,9 +7,9 @@ public class PowerupGeneration : MonoBehaviour
 {
     public GameObject[] PowerupList = new GameObject[Enum.GetNames(typeof(PowerupType)).Length];
     public GameObject ObjectToInstantiate;
-    public float firstPos = 40.0f;
-    public float minDist = 15.0f;
-    public float maxDist = 25.0f;
+    public float firstPos = 75.0f;
+    public float minDist = 25.0f;
+    public float maxDist = 30.0f;
     public float targetPos;
     public Vector3 currPosToGenerate = Vector3.zero;
 
